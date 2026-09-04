9417111cf1c079b5d96f286518f1fddf358c3743
xDarkixx <andygeiffes35@gmail.com>
2026-09-04 14:24:04 +0100

    chore: remove Windows deployment files

--- comfyui/start-windows.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference='Stop'
-$root=Split-Path -Parent $MyInvocation.MyCommand.Path
-$comfy=Join-Path $root 'ComfyUI'
-if(-not (Test-Path $comfy)){ throw 'ComfyUI is not installed. Run install-windows.ps1 first.' }
-Set-Location $comfy
-$python=if($env:COMFYUI_PYTHON){$env:COMFYUI_PYTHON}else{'python'}
-$hostName=if($env:COMFYUI_HOST){$env:COMFYUI_HOST}else{'127.0.0.1'}
-$port=if($env:COMFYUI_PORT){$env:COMFYUI_PORT}else{'8188'}
-& $python main.py --listen $hostName --port $port
